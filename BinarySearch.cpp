56771a6b764001a5b83166ee45487edc9d1611f0
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-07 12:28:07 +0700

    commit6

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,13 +24,26 @@ void input()
     }
 
     cout << "\n ====================================\n";
-    cout << "            Masukkan Element Array    \n";
+    cout << "            Masukkan Element Array  \n";
     cout << "====================================\n";
     for (int i = 0; i < nPanjang; i++)
     {
         cout << "Data ke-" << (i + 1) << " = ";
         cin >> element[i];
     }
+}
+
+void binarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n=============================\n";
+        cout <<
+    }
+        /* code */
+    
+    
 }
     
     
