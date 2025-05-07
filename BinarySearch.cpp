3b1cb018ecfeab1c8c3240b2c2d92e330f0316f4
DzakwanPw <dzakwan.putra.ft24@mail.umy.ac.id>
2025-05-07 12:17:18 +0700

    commit2

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,6 +1,32 @@
+
+
 #include <iostream>
 using namespace std;
 
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    whilte(true)
+    {
+        cout << "Masukkan banyaknya element pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah element tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
+
+    
+}
+    
+    
+        
+
+    
